c7433945272ec9a18348d88f65b22d4b653a9238
Esteban-jv <ejv.developer@gmail.com>
2021-12-28 07:11:33 -0600

    store method

--- ecomerce_rest/apps/users/api/api.py
+++ ecomerce_rest/apps/users/api/api.py
@@ -11,3 +11,11 @@ def user_api_view(request):
         users = User.objects.all()
         users_serializers = UserSerializer(users, many=True)
         return Response(users_serializers.data)
+
+    if request.method == 'POST':
+        user_serializer = UserSerializer(data=request.data)
+        if user_serializer.is_valid():
+            user_serializer.save()
+            return Response(user_serializer.data)
+        else:
+            return Response(user_serializer.errors)
