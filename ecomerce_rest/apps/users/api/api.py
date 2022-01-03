0a87538f2d0207c1ee82967424af71a26a0ea032
Esteban-jv <ejv.developer@gmail.com>
2022-01-03 07:25:22 -0600

    to_representation serializer

--- ecomerce_rest/apps/users/api/api.py
+++ ecomerce_rest/apps/users/api/api.py
@@ -3,14 +3,14 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework.decorators import api_view
 from apps.users.models import User
-from apps.users.api.serializers import UserSerializer, TestUserSerializer
+from apps.users.api.serializers import UserSerializer #, TestUserSerializer
 
 @api_view(['GET','POST'])
 def user_api_view(request):
 
     # List
     if request.method == 'GET':
-        users = User.objects.all()
+        users = User.objects.all().values('id','username','email','name','last_name')
         users_serializers = UserSerializer(users, many=True)
         return Response(users_serializers.data, status=status.HTTP_200_OK)
 
@@ -33,7 +33,7 @@ def user_detail_api_view(request, pk=None):
         # Update
         elif request.method == 'PUT':
 
-            user_serializer = TestUserSerializer(user, data=request.data)
+            user_serializer = UserSerializer(user, data=request.data)
             if user_serializer.is_valid():
                 user_serializer.save()
                 return Response(user_serializer.data, status=status.HTTP_200_OK)
--- ecomerce_rest/apps/users/api/serializers.py
+++ ecomerce_rest/apps/users/api/serializers.py
@@ -6,46 +6,56 @@ class UserSerializer(serializers.ModelSerializer):
         model = User
         fields = '__all__'
         # exclude
+    def to_representation(self, instance):
+        # data = super().to_representation(instance)
+        # print(data)
+        return { # Cada una de estas claves pueden tener otro nombre sin pedos
+            'id': instance['id'],
+            'username': instance['username'],
+            'email': instance['email'],
+            'name': instance['name'],
+            'last_name': instance['last_name'],
+        }
 
-class TestUserSerializer(serializers.Serializer):
-    name = serializers.CharField(max_length=200)
-    email = serializers.EmailField()
+# class TestUserSerializer(serializers.Serializer):
+#     name = serializers.CharField(max_length=200)
+#     email = serializers.EmailField()
 
     # Validación relacionada al nombre
-    def validate_name(self, value):
-        # custom validation
-        if 'developer' in value:
-            raise serializers.ValidationError('Error not develop')
-        return value
+    # def validate_name(self, value):
+    #     # custom validation
+    #     if 'developer' in value:
+    #         raise serializers.ValidationError('Error not develop')
+    #     return value
 
     # Validación relacionada al correo
-    def validate_email(self, value):
-        # custom validate
-        if value == '':
-            raise serializers.ValidationError('No puede ir vacío')
+    # def validate_email(self, value):
+    #     # custom validate
+    #     if value == '':
+    #         raise serializers.ValidationError('No puede ir vacío')
 
         # if self.context['name'] in value:
         # if self.validate_name(self.context['name']) in value: # Forza la validacion de name para poder continuar con el código de abajo
         #     raise serializers.ValidationError('El email no puede contener el nombre')
 
-        return value
+        # return value
 
     # validación no relacionada a ningun campo
-    def validate(self, data):
-        if data['name'] in data['email']:
-            raise serializers.ValidationError('El email no puede contener el nombre')
-        return data
+    # def validate(self, data):
+    #     if data['name'] in data['email']:
+    #         raise serializers.ValidationError('El email no puede contener el nombre')
+    #     return data
 
     # this method calls with serializer.save
-    def create(self, validated_data):
-        print(validated_data)
-        return User.objects.create(**validated_data)
+    # def create(self, validated_data):
+    #     print(validated_data)
+    #     return User.objects.create(**validated_data)
 
-    def update(self, instance, validated_data):
-        instance.name = validated_data.get('name', instance.name)
-        instance.email = validated_data.get('email', instance.email)
-        instance.save() # Este save está dentro del modelo
-        return instance
+    # def update(self, instance, validated_data):
+    #     instance.name = validated_data.get('name', instance.name)
+    #     instance.email = validated_data.get('email', instance.email)
+    #     instance.save() # Este save está dentro del modelo
+    #     return instance
 
     # def save(self):
     #     send_email(self.validated_data) # No guarda en la base de datos
