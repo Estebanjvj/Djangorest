1d7386d55843206d3d14d269fc1f7228c159cf77
Esteban-jv <ejv.developer@gmail.com>
2022-01-14 12:11:38 -0600

    3 Ways to show related fields in serializers

--- ecomerce_rest/apps/products/api/serializers/product_serializers.py
+++ ecomerce_rest/apps/products/api/serializers/product_serializers.py
@@ -1,8 +1,24 @@
-from apps.products.models import Product
 from rest_framework import serializers
 
+from apps.products.models import Product
+from apps.products.api.serializers.general_serializers import MeasureUnitSerializer, CategoryProductSerializer
+
 class ProductSerializer(serializers.ModelSerializer):
+    # measure_unit = MeasureUnitSerializer()
+    # category_product = CategoryProductSerializer()
+
+    # measure_unit = serializers.StringRelatedField() #Model.__str()
+    # category_product = serializers.StringRelatedField() #Model.__str()
 
     class Meta:
         model = Product
-        exclude = ('status',)
+        exclude = ('status','created_date','modificated_date','deleted_date')
+
+    def to_representation(self, instance):
+        return {
+            'id': instance.id,
+            'description': instance.description,
+            'image': instance.image if instance.image else '',
+            'measure_unit': instance.measure_unit.description,
+            'category_product': instance.category_product.description,
+        }
