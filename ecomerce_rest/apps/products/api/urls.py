682ec4e23de10639e7f3c6dc3810e9323af99470
Esteban-jv <ejv.developer@gmail.com>
2022-02-13 15:37:16 -0500

    show and desrpoy product

--- ecomerce_rest/apps/products/api/urls.py
+++ ecomerce_rest/apps/products/api/urls.py
@@ -1,11 +1,18 @@
 from django.urls import path
 from apps.products.api.views.general_views import MeasureUnitListAPIView, IndicatorListAPIView, CategoryProductListAPIView
-from apps.products.api.views.product_views import ProductListAPIView, ProductCreateAPIView
+from apps.products.api.views.product_views import (
+    ProductListAPIView,
+    ProductCreateAPIView,
+    ProductRetrieveApiView,
+    ProductDestroyAPIView
+)
 
 urlpatterns = [
     path('measure_unit/',MeasureUnitListAPIView.as_view(), name='measure_unit'),
     path('indicator/',IndicatorListAPIView.as_view(), name='indicator'),
     path('category_product/',CategoryProductListAPIView.as_view(), name='category_product'),
     path('product/list/',ProductListAPIView.as_view(), name='product_list'),
-    path('product/create/',ProductCreateAPIView.as_view(), name='product_create')
+    path('product/create/',ProductCreateAPIView.as_view(), name='product_create'),
+    path('product/retrieve/<int:pk>',ProductRetrieveApiView.as_view(), name='product_retrieve'),
+    path('product/destroy/<int:pk>',ProductDestroyAPIView.as_view(), name='product_destroy')
 ]
--- ecomerce_rest/apps/products/api/views/product_views.py
+++ ecomerce_rest/apps/products/api/views/product_views.py
@@ -1,5 +1,40 @@
+from rest_framework import generics
+from rest_framework import status
+from rest_framework.response import Response
+
 from apps.base.api import GeneralListApiView
 from apps.products.api.serializers.product_serializers import ProductSerializer
 
 class ProductListAPIView(GeneralListApiView):
     serializer_class = ProductSerializer
+
+class ProductCreateAPIView(generics.CreateAPIView):
+    serializer_class = ProductSerializer
+
+    def post(self, request, *args, **kwargs):
+        serializer = self.serializer_class(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response({'message': 'Producto creado correctamente'},status=status.HTTP_201_CREATED)
+        return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
+
+class ProductRetrieveApiView(generics.RetrieveAPIView):
+    serializer_class = ProductSerializer
+
+    def get_queryset(self):
+        return self.get_serializer().Meta.model.objects.filter(status=True)
+
+class ProductDestroyAPIView(generics.DestroyAPIView):
+    serializer_class = ProductSerializer
+
+    def get_queryset(self):
+        return self.get_serializer().Meta.model.objects.filter(status=True)
+
+    def delete(self, request, pk=None):
+        # soft delete al reescribir esto
+        product = self.get_queryset().filter(id=pk).first()
+        if product:
+            product.status = False
+            product.save()
+            return Response({'msg':'Producto eliminado correctamente'}, status=status.HTTP_200_OK)
+        return Response({'error':'No existe un producto con estos datos'}, status=status.HTTP_400_BAD_REQUEST)
